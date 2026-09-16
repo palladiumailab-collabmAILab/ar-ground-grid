c30d555e91b174e4127f9766d1b83eb1c212bd7a
palladiumailab <palladium.ai.lab@gmail.com>
2026-09-16 17:16:16 +0900

    build: compile against Android API 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.palladiumailab.argroundgrid"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.palladiumailab.argroundgrid"
